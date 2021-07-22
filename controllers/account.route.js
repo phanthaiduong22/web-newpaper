2b98dcba08878dd70db4334edd076c3521c973c3
tieubaovi0217 <nguyenvitieubao0217@gmail.com>
2021-07-22 10:31:27 +0700

    tam on

--- controllers/account.route.js
+++ controllers/account.route.js
@@ -4,7 +4,7 @@ const moment = require("moment");
 
 const userModel = require("../models/user.model");
 const resetModel = require("../models/reset.model");
-const { authUser, authRole } = require("../middlewares/auth.mdw");
+const { authUser, authRole, notAuth } = require("../middlewares/auth.mdw");
 
 const sendEmail = require("../utils/sendEmail");
 
@@ -14,13 +14,13 @@ router.get("/profile", authUser, authRole("user"), function (req, res) {
   res.render("vwAccount/profile");
 });
 
-router.get("/register", function (req, res) {
+router.get("/register", notAuth, function (req, res) {
   res.render("vwAccount/register", {
     active: { register: true },
   });
 });
 
-router.post("/register", async function (req, res) {
+router.post("/register", notAuth, async function (req, res) {
   const hash = bcrypt.hashSync(req.body.raw_password, 10);
   const dob = moment(req.body.raw_dob, "DD/MM/YYYY").format("YYYY-MM-DD");
   const user = {
@@ -61,25 +61,27 @@ router.get("/is-valid-email", async function (req, res) {
   res.json(true);
 });
 
-router.get("/login", async function (req, res) {
+router.get("/login", notAuth, async function (req, res) {
   res.render("vwAccount/login", {
     active: { login: true },
   });
 });
 
-router.post("/login", async function (req, res) {
+router.post("/login", notAuth, async function (req, res) {
+  if (req.session.auth === true) {
+    return res.redirect("/");
+  }
   const user = await userModel.findByUsername(req.body.username);
   if (user === null) {
     return res.render("vwAccount/login", {
-      err_message: "Invalid username!",
+      err_message: "No account with that username!",
     });
   }
 
   const ret = bcrypt.compareSync(req.body.password, user.Password);
   if (ret === false) {
     return res.render("vwAccount/login", {
-      layout: false,
-      err_message: "Invalid password!",
+      err_message: "Incorrect password!",
     });
   }
 
@@ -100,18 +102,17 @@ router.post("/logout", authUser, async function (req, res) {
   res.redirect(url);
 });
 
-router.get("/resetpassword", async (req, res) => {
+router.get("/resetpassword", notAuth, async (req, res) => {
   res.render("vwAccount/resetpassword");
 });
 
-router.get("/otp", async (req, res) => {
+router.get("/otp", notAuth, async (req, res) => {
   res.render("vwAccount/otp");
 });
 
-router.post("/otp/verify", async (req, res) => {
+router.post("/otp/verify", notAuth, async (req, res) => {
   const { email, otp } = req.body;
-  console.log("email:", email);
-  console.log("otp:", otp);
+  console.log(email, otp);
   const reset = await resetModel.findByEmail(email);
   const timeNow = new Date().getTime();
   console.log(reset, timeNow);
@@ -125,10 +126,13 @@ router.post("/otp/verify", async (req, res) => {
     return res.render("vwAccount/resetpassword", { email, otp, edit: true });
   }
   console.log("otp was not match or expired.");
-  res.render("vwAccount/resetpassword", { email });
+  res.render("vwAccount/otp", {
+    email,
+    err_message: "OTP was not match or expired.",
+  });
 });
 
-router.post("/resetpassword", async (req, res) => {
+router.post("/resetpassword", notAuth, async (req, res) => {
   const email = req.body.email;
   const otp = Math.floor(Math.random() * 900000) + 100000;
   console.log(email, otp);
@@ -136,6 +140,18 @@ router.post("/resetpassword", async (req, res) => {
   res.render("vwAccount/otp", { email });
 });
 
-router.put("/resetpassword", async (req, res) => {});
+router.post("/resetpassword/change", notAuth, async (req, res) => {
+  const { password, otp, email } = req.body;
+  console.log(password, otp, email);
+  const user = await userModel.findByEmail(email);
+  const reset = await resetModel.findByEmail(email);
+  console.log(user, reset);
+  if (user.Email === email && reset.otp == otp) {
+    const hash = bcrypt.hashSync(password, 10);
+    userModel.changePassword(user.UserID, hash);
+    return res.redirect("/account/login");
+  }
+  return res.redirect("/");
+});
 
 module.exports = router;
