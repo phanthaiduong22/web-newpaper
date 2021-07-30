9d7aff6278fc837485419d4485178b44f511d293
doodaljin <doodal.jin@gmail.com>
2021-07-30 20:06:09 +0700

    add sub category

--- controllers/category.route.js
+++ controllers/category.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { authUser, authRole } = require("../middlewares/auth.mdw");
 const categoryModel = require("../models/category.model");
-const { route } = require("./editor.route");
 
 const router = express.Router();
 
@@ -49,5 +48,26 @@ router.post("/del", authUser, authRole("admin"), async function (req, res) {
   await categoryModel.del(req.body.CatID);
   res.redirect("/admin/categories");
 });
+
+router.get(
+  "/:id/addSubCategory",
+  authUser,
+  authRole("admin"),
+  async function (req, res) {
+    res.render("vwCategories/addSubCat");
+  },
+);
+
+router.post(
+  "/:id/addSubCategory",
+  authUser,
+  authRole("admin"),
+  async function (req, res) {
+    const subCatName = req.body.txtSubCatName;
+    const catId = req.params.id;
+    await categoryModel.addSubCat(subCatName, catId);
+    res.redirect("/admin/categories");
+  },
+);
 
 module.exports = router;
