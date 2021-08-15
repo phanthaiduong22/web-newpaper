58250483278bf5abbe0d316359b1f36e81f83af0
tieubaovi0217 <nguyenvitieubao0217@gmail.com>
2021-08-15 19:40:24 +0700

    done

--- controllers/tag.route.js
+++ controllers/tag.route.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const { authUser, authRole } = require("../middlewares/auth.mdw");
-const paperModel = require("../models/paper.model");
-const tagModel = require("../models/tag.model");
-const moment = require("moment");
+const express = require('express');
+const { authUser, authRole } = require('../middlewares/auth.mdw');
+const paperModel = require('../models/paper.model');
+const tagModel = require('../models/tag.model');
+const moment = require('moment');
 const router = express.Router();
 
-const { sortHelper } = require("../helpers/sort");
+const { sortHelper } = require('../helpers/sort');
 
-router.get("/", async (req, res) => {
-  if (req.query.hasOwnProperty("name")) {
+router.get('/', async (req, res) => {
+  if (req.query.hasOwnProperty('name')) {
     const tagName = req.query.name;
     const tag = await tagModel.findTagIdByTagName(tagName);
     return res.redirect(`/tags/find/${tag.TagId}`);
@@ -17,17 +17,17 @@ router.get("/", async (req, res) => {
   // for (let tag of tags) {
   //   tag.count = await tagModel.countPaperByTagName(tag.TagName);
   // }
-  res.render("vwTags/index", { tags, active: { tagManagement: true } });
+  res.render('vwTags/index', { tags, active: { tagManagement: true } });
 });
 
-router.get("/find/:id", async (req, res) => {
+router.get('/find/:id', async (req, res) => {
   let tagId = +req.params.id;
   const page = +req.query.page || 1;
   if (page < 1) page = 1;
   const limit = 3;
   const tag = await tagModel.findTagById(tagId);
   if (!tag) {
-    return res.redirect("/tags");
+    return res.redirect('/tags');
   }
   const total = await paperModel.countByTagId(tagId);
 
@@ -46,9 +46,9 @@ router.get("/find/:id", async (req, res) => {
   const papers = await paperModel.findPapersByTagId(tagId, limit, offset);
 
   for (let i of papers) {
-    i.PublishDate = moment(i.PublishDate).format("Do MMMM YYYY");
+    i.PublishDate = moment(i.PublishDate).format('Do MMMM YYYY');
   }
-  res.render("vwTags/list", {
+  res.render('vwTags/list', {
     papers,
     page_numbers,
     previous_page: page > 1 ? page - 1 : undefined,
@@ -57,35 +57,35 @@ router.get("/find/:id", async (req, res) => {
   });
 });
 
-router.get("/add", authUser, authRole("admin"), async (req, res) => {
-  res.render("vwTags/add", { active: { tagManagement: true } });
+router.get('/add', authUser, authRole('admin'), async (req, res) => {
+  res.render('vwTags/add', { active: { tagManagement: true } });
 });
 
-router.post("/add", authUser, authRole("admin"), async (req, res) => {
+router.post('/add', authUser, authRole('admin'), async (req, res) => {
   const tagName = req.body.txtTagName;
   const result = await tagModel.addTag({ TagName: tagName });
   if (!result)
-    return res.render("vwTags/add", { err_message: "This tag is used." });
-  res.redirect("/tags");
+    return res.render('vwTags/add', { err_message: 'This tag is used.' });
+  res.redirect('/tags');
 });
 
-router.get("/edit", authUser, authRole("admin"), async (req, res) => {
+router.get('/edit', authUser, authRole('admin'), async (req, res) => {
   const tagId = +req.query.id || 1;
   const tag = await tagModel.findTagById(tagId);
 
-  if (!tag) return res.redirect("/tags");
+  if (!tag) return res.redirect('/tags');
 
-  res.render("vwTags/edit", { tag, active: { tagManagement: true } });
+  res.render('vwTags/edit', { tag, active: { tagManagement: true } });
 });
 
-router.post("/patch", authUser, authRole("admin"), async (req, res) => {
+router.post('/patch', authUser, authRole('admin'), async (req, res) => {
   await tagModel.patch(req.body);
-  res.redirect("/tags");
+  res.redirect('/tags');
 });
 
-router.post("/del", authUser, authRole("admin"), async (req, res) => {
+router.post('/del', authUser, authRole('admin'), async (req, res) => {
   await tagModel.del(req.body);
-  res.redirect("/tags");
+  res.redirect('/tags');
 });
 
 module.exports = router;
