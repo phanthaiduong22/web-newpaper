19ad8a9a7a70ed95da77d604eb3c7cf140370105
tieubaovi0217 <nguyenvitieubao0217@gmail.com>
2021-08-15 15:45:47 +0700

    add gom nhom theo status Writer

--- controllers/paper.route.js
+++ controllers/paper.route.js
@@ -1,20 +1,20 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 
-const commentModel = require('../models/comment.model');
-const paperModel = require('../models/paper.model');
-const categoryModel = require('../models/category.model');
-const moment = require('moment');
-const puppeteer = require('puppeteer');
-const { authUser, authPremium } = require('../middlewares/auth.mdw');
+const commentModel = require("../models/comment.model");
+const paperModel = require("../models/paper.model");
+const categoryModel = require("../models/category.model");
+const moment = require("moment");
+const puppeteer = require("puppeteer");
+const { authUser, authPremium } = require("../middlewares/auth.mdw");
 
 const DOMAIN = process.env.DOMAIN;
 
 const router = express.Router();
 
-router.get('/byCat/:id', async function (req, res) {
+router.get("/byCat/:id", async function (req, res) {
   const catId = +req.params.id || 0;
 
   for (c of res.locals.lcCategories) {
@@ -59,67 +59,76 @@ router.get('/byCat/:id', async function (req, res) {
   }
 
   for (let i of list) {
-    i.PublishDate = moment(i.PublishDate).format('Do MMMM YYYY');
+    i.PublishDate = moment(i.PublishDate).format("Do MMMM YYYY");
   }
-  res.render('vwPapers/byCat', {
+  res.render("vwPapers/byCat", {
     papers: list,
     categories: categories,
     empty: list.length === 0,
     page_numbers,
     previous_page: page > 1 ? page - 1 : undefined,
     next_page: page < page_numbers.length ? page + 1 : undefined,
-    layout: 'categories.hbs',
+    layout: "categories.hbs",
     active: { categories: true },
     CatActive: catId,
     SubCatActive: 0,
   });
 });
 
-router.get('/details/:id', async function (req, res) {
+router.get("/details/:id", async function (req, res) {
   const paperId = +req.params.id || 0;
   const paper = await paperModel.findById(paperId);
-  if (paper === null) return res.redirect('/');
+  if (paper === null) return res.redirect("/");
 
   if (paper.Premium) {
     if (!req.session.authUser || !req.session.authUser.Premium) {
-      return res.render('vwPapers/details', {
-        err_message: 'This paper is premium!',
+      return res.render("vwPapers/details", {
+        err_message: "This paper is premium!",
       });
     }
   }
   const relatedNews = await paperModel.findByCatID(paper.CatID);
 
   paperModel.increaseView(paperId, paper.Views);
-  paper.CreatedAt = moment(paper.CreatedAt).format('Do MMMM YYYY');
-  paper.PublishDate = moment(paper.PublishDate).format('Do MMMM YYYY');
+  paper.CreatedAt = moment(paper.CreatedAt).format("Do MMMM YYYY");
+  paper.PublishDate = moment(paper.PublishDate).format("Do MMMM YYYY");
   for (i = 0; i < relatedNews.length; i++) {
     relatedNews[i].CreatedAt = moment(relatedNews[i].CreatedAt).format(
-      'Do MMMM YYYY',
+      "Do MMMM YYYY",
     );
     relatedNews[i].PublishDate = moment(relatedNews[i].PublishDate).format(
-      'Do MMMM YYYY',
+      "Do MMMM YYYY",
     );
   }
 
-  const comments = await commentModel.findAllCommentByPaperId(paperId);
+  let comments = await commentModel.findAllCommentByPaperId(paperId);
+  let guestComments = await commentModel.findGuestCommentsByPaperId(paperId);
+  guestComments = guestComments.map((comment) => {
+    return {
+      ...comment,
+      Name: "anonymous",
+      Role: "guest",
+    };
+  });
+  comments = [...comments, ...guestComments];
   for (i = 0; i < comments.length; i++) {
     comments[i].rawCreatedAt = comments[i].CreatedAt;
     comments[i].CreatedAt = moment(comments[i].CreatedAt).format(
-      'Do MMMM YYYY',
+      "Do MMMM YYYY",
     );
   }
   // console.log(comments);
-  res.render('vwPapers/details', {
+  res.render("vwPapers/details", {
     paper,
     relatedNews,
     comments,
   });
 });
 
-router.get('/bySubCat/:subcatid', async function (req, res) {
+router.get("/bySubCat/:subcatid", async function (req, res) {
   const subCatId = +req.params.subcatid || 0;
   const cat = await categoryModel.getCatbySubCatID(subCatId);
-  if (cat === null) return res.redirect('/');
+  if (cat === null) return res.redirect("/");
 
   const limit = 3;
   const page = req.query.page || 1;
@@ -156,15 +165,15 @@ router.get('/bySubCat/:subcatid', async function (req, res) {
   }
 
   for (let i of list) {
-    i.PublishDate = moment(i.PublishDate).format('Do MMMM YYYY');
+    i.PublishDate = moment(i.PublishDate).format("Do MMMM YYYY");
   }
 
-  res.render('vwPapers/byCat', {
+  res.render("vwPapers/byCat", {
     papers: list,
     categories: categories,
     empty: list.length === 0,
     page_numbers,
-    layout: 'categories.hbs',
+    layout: "categories.hbs",
     active: { categories: true },
     CatActive: cat[0].CatID,
     SubCatActive: subCatId,
@@ -172,56 +181,56 @@ router.get('/bySubCat/:subcatid', async function (req, res) {
 });
 
 router.get(
-  '/details/:id/premium',
+  "/details/:id/premium",
   authUser,
   authPremium,
   async function (req, res) {
     const paperId = +req.params.id || 0;
     let paper = await paperModel.findById(paperId);
     if (!paper) {
-      return res.redirect('/');
+      return res.redirect("/");
     }
 
     paperModel.increaseView(paperId, paper.Views);
-    paper.PublishDate = moment(paper.PublishDate).format('Do MMMM YYYY');
+    paper.PublishDate = moment(paper.PublishDate).format("Do MMMM YYYY");
 
-    res.render('vwPapers/premium', { paper });
+    res.render("vwPapers/premium", { paper });
   },
 );
 
 router.get(
-  '/details/:id/download',
+  "/details/:id/download",
   authUser,
   authPremium,
   async function (req, res) {
     const paperId = +req.params.id || 0;
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
     const url = `http://${DOMAIN}/papers/details/${paperId}/premium`;
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
     await page.setCookie({
-      name: 'connect.sid',
-      value: req.cookies['connect.sid'],
+      name: "connect.sid",
+      value: req.cookies["connect.sid"],
       url,
     });
 
     const filename = `pdf/paper-${paperId}.pdf`;
     // const filePath = path.join(__dirname, "..", `pdf/test-16.pdf`);
-    const filePath = path.join(__dirname, '..', filename);
-    await page.goto(url, { waitUntil: 'networkidle2' });
-    await page.pdf({ path: filePath, format: 'a4' });
+    const filePath = path.join(__dirname, "..", filename);
+    await page.goto(url, { waitUntil: "networkidle2" });
+    await page.pdf({ path: filePath, format: "a4" });
     await browser.close();
 
     res.download(filePath);
   },
 );
 
-router.post('/details/:id/comment', authUser, async function (req, res) {
+router.post("/details/:id/comment", async function (req, res) {
   const { content } = req.body;
-  const userId = req.session.authUser.UserID;
+  const userId = req.session.authUser ? req.session.authUser.UserID : "guest";
   const paperId = +req.params.id || 0;
   const comment = {
     PaperID: paperId,
