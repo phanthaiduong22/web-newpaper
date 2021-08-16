98fead7f121ffef506ef168641a56d72fbefec86
doodaljin <doodal.jin@gmail.com>
2021-08-16 20:00:16 +0700

    fix responsive writer upload

--- controllers/search.route.js
+++ controllers/search.route.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const paperModel = require("../models/paper.model");
-const moment = require("moment");
+const express = require('express');
+const paperModel = require('../models/paper.model');
+const moment = require('moment');
 
 const router = express.Router();
 
-router.get("/", async function (req, res) {
-  res.render("Search Something");
+router.get('/', async function (req, res) {
+  res.render('Search Something');
 });
 
-router.post("/", async function (req, res) {
+router.post('/', async function (req, res) {
   const searchList = await paperModel.search(req.body.search);
 
   for (let i of searchList) {
-    i.PublishDate = moment(i.PublishDate).format("Do MMMM YYYY");
+    i.PublishDate = moment(i.PublishDate).format('Do MMMM YYYY');
   }
-  res.render("search", {
+  res.render('search', {
     searchList,
     content: req.body.search,
   });
