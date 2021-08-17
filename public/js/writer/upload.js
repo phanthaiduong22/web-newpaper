9453a3b9b85594c9e1340240d65c826d2402028b
doodaljin <doodal.jin@gmail.com>
2021-08-17 11:18:34 +0700

    fix login responsive

--- public/js/writer/upload.js
+++ public/js/writer/upload.js
@@ -1,10 +1,10 @@
 tinymce.init({
-  selector: "#txtContent",
+  selector: '#txtContent',
   plugins:
-    "a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker",
+    'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
   toolbar:
-    "a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table",
-  toolbar_mode: "floating",
-  tinycomments_mode: "embedded",
-  tinycomments_author: "Author name",
+    'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+  toolbar_mode: 'floating',
+  tinycomments_mode: 'embedded',
+  tinycomments_author: 'Author name',
 });
