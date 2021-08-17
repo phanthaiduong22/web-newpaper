a32044ad21bb4d422d514ca90d1fb056f093eb5d
doodaljin <doodal.jin@gmail.com>
2021-08-17 21:17:49 +0700

    add extend premium for users/admin role

--- public/js/admin/users.js
+++ public/js/admin/users.js
@@ -1,13 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var deleteUserForm = document.getElementById('delete-user-form');
-  var deleteUserBtn = document.getElementById('delete-user-btn');
+document.addEventListener("DOMContentLoaded", function () {
+  var deleteUserForm = document.getElementById("delete-user-form");
+  var deleteUserBtn = document.getElementById("delete-user-btn");
+  var extendBtn = document.getElementById("extend-btn");
+  var extendForm = document.getElementById("extend-form");
   var id;
-  $('#delete-course-modal').on('show.bs.modal', function (event) {
+  $("#delete-course-modal").on("show.bs.modal", function (event) {
     var button = $(event.relatedTarget);
-    id = button.data('id');
+    id = button.data("id");
   });
-  deleteUserBtn.addEventListener('click', function () {
+  deleteUserBtn.addEventListener("click", function () {
     deleteUserForm.action = `/admin/users/${id}/del`;
     deleteUserForm.submit();
+  });
+
+  $("#extend").on("show.bs.modal", function (event) {
+    var button = $(event.relatedTarget);
+    id = button.data("id");
+  });
+  extendBtn.addEventListener("click", function () {
+    extendForm.action = `/account/profile/extend?id=${id}`;
+    extendForm.submit();
   });
 });
